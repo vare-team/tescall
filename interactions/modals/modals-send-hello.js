689f17bc4d575002ecdd5538fd15d8a3f657a298
Alexander <satan1c@ukr.net>
2022-08-11 17:33:47 +0300

    add success modals response

---
+++ interactions/modals/modals-send-hello.js
@@ -0,0 +1,18 @@
+import { MessageEmbed } from 'discord.js';
+import { colors, messages } from '../../config.js';
+
+export default async function (interaction) {
+	await interaction.reply({
+		embeds: [
+			new MessageEmbed()
+				.setTitle(messages.hello.replace('%NAME%', interaction.user.username))
+				.setDescription(messages.helloDescription)
+				.setColor(
+					mainGuild.members.cache.get(interaction.user.id) ??
+						(await mainGuild.members.fetch(interaction.user.id))?.roles.cache.has(process.env.ROLE)
+						? colors.yellow
+						: colors.blue
+				),
+		],
+	});
+}
