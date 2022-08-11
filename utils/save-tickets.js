c2d4fa407c701e631b4190965d8d205a93993c4f
Alexander <satan1c@ukr.net>
2022-08-11 17:31:36 +0300

    add mutes.json create/save

--- utils/save-tickets.js
+++ utils/save-tickets.js
@@ -8,12 +8,17 @@ export default function () {
 	writeFile('./threads.json', JSON.stringify(Object.fromEntries(threads), null, 2), { flag: 'w' }, e => {
 		if (e) console.warn(e);
 	});
+
+	writeFile('./mutes.json', JSON.stringify(Object.fromEntries(mutes), null, 2), { flag: 'w' }, e => {
+		if (e) console.warn(e);
+	});
 }
 
 export function createFiles() {
 	try {
 		writeFileSync('./tickets.json', '{}', { flag: 'wx' });
 		writeFileSync('./threads.json', '{}', { flag: 'wx' });
+		writeFileSync('./mutes.json', '{}', { flag: 'wx' });
 		// eslint-disable-next-line no-empty
 	} catch (e) {}
 }
