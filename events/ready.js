80aa51d664794ee7e15057cdb8f0b03ca3b371f1
Alexander <satan1c@ukr.net>
2023-02-17 19:27:10 +0200

    permissions flags fix

--- events/ready.js
+++ events/ready.js
@@ -6,25 +6,24 @@ import messageUpdateEvent from './message-update.js';
 import messageDeleteEvent from './message-delete.js';
 import interactionCreateEvent from './interaction-create.js';
 import threadDeleteEvent from './thread-delete.js';
-import { ActivityType } from 'discord.js';
 
-export default async function (client) {
+export default async function () {
 	log('Авторизация выполнена!');
 
-	global.mainGuild = await client.guilds.fetch(process.env.GUILD);
+	global.mainGuild = await discordClient.guilds.fetch(process.env.GUILD);
 	if (process.env.ROLE) global.boosterRole = await mainGuild.roles.fetch(process.env.ROLE);
 	global.ticketsChannel = await mainGuild.channels.fetch(process.env.CHANNEL);
 
-	await client.application.commands.set(Object.values(commands).slice(1));
+	await discordClient.application.commands.set(Object.values(commands).slice(1));
 
-	client.on('messageCreate', messageCreateEvent);
-	client.on('messageUpdate', messageUpdateEvent);
-	client.on('messageDelete', messageDeleteEvent);
-	client.on('interactionCreate', interactionCreateEvent);
-	client.on('threadDelete', threadDeleteEvent);
+	discordClient.on('messageCreate', messageCreateEvent);
+	discordClient.on('messageUpdate', messageUpdateEvent);
+	discordClient.on('messageDelete', messageDeleteEvent);
+	discordClient.on('interactionCreate', interactionCreateEvent);
+	discordClient.on('threadDelete', threadDeleteEvent);
 
-	client.user.setActivity('Напиши в ЛС для помощи!', { type: ActivityType.Watching });
+	discordClient.user.setActivity('Напиши в ЛС для помощи!', { type: 'Watching' });
 
-	log(`Лог канал закеширован! #${client.channels.cache.get(process.env.CHANNEL).name}`);
+	log(`Лог канал закеширован! #${discordClient.channels.cache.get(process.env.CHANNEL).name}`);
 	log('К работе готов!\n');
 }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ if (!process.env.WEBHOOK_URL || !process.env.TOKEN || !process.env.CHANNEL || !p
 	process.exit();
 }
 
-import { Client, IntentsBitField, WebhookClient } from 'discord.js';
+import { Client, Partials, WebhookClient } from 'discord.js';
 import { createFiles, readFiles } from './utils/save-tickets.js';
 import log from './utils/log.js';
 import readyEvent from './events/ready.js';
@@ -11,12 +11,8 @@ import readyEvent from './events/ready.js';
 createFiles();
 
 const client = new Client({
-	intents:
-		IntentsBitField.Flags.Guilds |
-		IntentsBitField.Flags.DirectMessages |
-		IntentsBitField.Flags.GuildMessages |
-		IntentsBitField.Flags.GuildMembers,
-	partials: ['CHANNEL'],
+	intents: ['Guilds', 'DirectMessages', 'GuildMessages', 'GuildMembers'],
+	partials: [Partials.Channel],
 });
 
 global.discordClient = client;
