0de448713715c8816a3c72c15b6fc956ded3c206
Alexander <satan1c@ukr.net>
2023-02-17 18:30:35 +0200

    mute command can mute forever

--- config.js
+++ config.js
@@ -34,6 +34,7 @@ export const messages = {
 export const ticketsErrors = {
 	invalidBotId: 'Указан не верный айди',
 	muted: 'Вы временно не можете создавать новых обращений',
+	mutedForever: 'Вы не можете создавать новых обращений',
 	unavailableDm: 'Не возможно отправить сообщение этому пользователю.',
 };
 
@@ -144,7 +145,7 @@ export const commands = {
 			new SlashCommandStringOption()
 				.setName('time')
 				.setRequired(true)
-				.setDescription('длительность мута, в формате 1h и т.д.')
+				.setDescription('длительность мута, в ms формате 1h и т.д., или -1 для мута навсегда')
 		)
 		.setDMPermission(false)
 		.toJSON(),
