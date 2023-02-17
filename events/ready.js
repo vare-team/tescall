3d68dd21dc0abd3169735294f99881eecdd520a9
Alexander <satan1c@ukr.net>
2023-02-17 18:23:03 +0200

    d.js 14 refactoring

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,3 @@
-import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v10';
 import { commands } from '../config.js';
 
 import log from '../utils/log.js';
@@ -8,27 +6,25 @@ import messageUpdateEvent from './message-update.js';
 import messageDeleteEvent from './message-delete.js';
 import interactionCreateEvent from './interaction-create.js';
 import threadDeleteEvent from './thread-delete.js';
+import { ActivityType } from 'discord.js';
 
-export default async function () {
+export default async function (client) {
 	log('Авторизация выполнена!');
 
-	global.mainGuild = await discordClient.guilds.fetch(process.env.GUILD);
+	global.mainGuild = await client.guilds.fetch(process.env.GUILD);
 	if (process.env.ROLE) global.boosterRole = await mainGuild.roles.fetch(process.env.ROLE);
 	global.ticketsChannel = await mainGuild.channels.fetch(process.env.CHANNEL);
 
-	const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-	await rest.put(Routes.applicationCommands(discordClient.application.id), {
-		body: Object.values(commands).slice(1),
-	});
+	await client.application.commands.set(Object.values(commands).slice(1));
 
-	discordClient.on('messageCreate', messageCreateEvent);
-	discordClient.on('messageUpdate', messageUpdateEvent);
-	discordClient.on('messageDelete', messageDeleteEvent);
-	discordClient.on('interactionCreate', interactionCreateEvent);
-	discordClient.on('threadDelete', threadDeleteEvent);
+	client.on('messageCreate', messageCreateEvent);
+	client.on('messageUpdate', messageUpdateEvent);
+	client.on('messageDelete', messageDeleteEvent);
+	client.on('interactionCreate', interactionCreateEvent);
+	client.on('threadDelete', threadDeleteEvent);
 
-	discordClient.user.setActivity('Напиши в ЛС для помощи!', { type: 'WATCHING' });
+	client.user.setActivity('Напиши в ЛС для помощи!', { type: ActivityType.Watching });
 
-	log(`Лог канал закеширован! #${discordClient.channels.cache.get(process.env.CHANNEL).name}`);
+	log(`Лог канал закеширован! #${client.channels.cache.get(process.env.CHANNEL).name}`);
 	log('К работе готов!\n');
 }
