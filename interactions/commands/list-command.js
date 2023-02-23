287aaa85a95c3c3d4d999dfa39e3a4261569b220
Alexander <satan1c@ukr.net>
2023-02-23 22:19:38 +0200

    lint fix

--- interactions/commands/list-command.js
+++ interactions/commands/list-command.js
@@ -1,11 +1,8 @@
 export default async function (interaction) {
 	interaction.reply({
-		content:
-			'Список тикетов:\n' +
-			[...tickets.entries()].reduce(
-				(p, [u, t], i) =>
-					`${p}\n${i + 1}. Взят: **${t.active}** Пользователь: <@${u}> (\`${u}\`) Тикет: <#${t.thread}>`,
-				''
-			),
+		content: `Список тикетов:\n${[...tickets.entries()].reduce(
+			(p, [u, t], i) => `${p}\n${i + 1}. Взят: **${t.active}** Пользователь: <@${u}> (\`${u}\`) Тикет: <#${t.thread}>`,
+			''
+		)}`,
 	});
 }
--- utils/log.js
+++ utils/log.js
@@ -1,16 +1,8 @@
 export default function (log) {
 	const date = new Date();
 	console.log(
-		`${
-			('00' + date.getDate()).slice(-2) +
-			'.' +
-			('00' + (date.getMonth() + 1)).slice(-2) +
-			' ' +
-			('00' + date.getHours()).slice(-2) +
-			':' +
-			('00' + date.getMinutes()).slice(-2) +
-			':' +
-			('00' + date.getSeconds()).slice(-2)
-		} : ${log}`
+		`${`${`00${date.getDate()}`.slice(-2)}.${`00${date.getMonth() + 1}`.slice(-2)} ${`00${date.getHours()}`.slice(
+			-2
+		)}:${`00${date.getMinutes()}`.slice(-2)}:${`00${date.getSeconds()}`.slice(-2)}`} : ${log}`
 	);
 }
