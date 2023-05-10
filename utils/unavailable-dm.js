e2e2361cb7ad8386e7cf06e478d2db717686b785
Alexander <satan1c@ukr.net>
2023-05-10 17:58:16 +0300

    dm error handler fix

--- utils/unavailable-dm.js
+++ utils/unavailable-dm.js
@@ -8,8 +8,8 @@ import getThread from './get-thread.js';
  */
 export default function (userId) {
 	return async () => {
-		const thread = await getThread(userId);
-		await thread.send(ticketsErrors.unavailableDm);
-		await closeTickets(thread.id)();
+		const thread = await getThread(userId).catch(() => {});
+		await thread?.send?.(ticketsErrors.unavailableDm).catch(() => {});
+		await closeTickets(thread?.id ?? tickets.get(userId).thread)();
 	};
 }
