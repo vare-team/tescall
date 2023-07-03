5d9b5eb80a12e10e8ff89c4d6fe908ebd7e3c198
Максим <53787123+IRoscom@users.noreply.github.com>
2023-07-04 03:28:07 +1000

    Added message url and operator "?" to the status.

--- interactions/commands/list-command.js
+++ interactions/commands/list-command.js
@@ -1,8 +1,14 @@
 export default async function (interaction) {
 	interaction.reply({
 		content: `Список тикетов:\n${[...tickets.entries()].reduce(
-			(p, [u, t], i) => `${p}\n${i + 1}. Взят: **${t.active}** Пользователь: <@${u}> (\`${u}\`) Тикет: <#${t.thread}>`,
+			(p, [u, t], i) =>
+				`${p}\n${i + 1}. Взят: **${
+					t.active ? 'Да' : 'Нет'
+				}** Пользователь: <@${u}> (\`${u}\`) Тикет: https://discord.com/channels/${mainGuild.id}/${ticketsChannel.id}/${
+					t.thread
+				}(<#${t.thread}>)`,
 			''
 		)}`,
+		flags: 'SuppressEmbeds',
 	});
 }
