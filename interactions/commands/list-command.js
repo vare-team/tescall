954276c3354244ae7570800f20fe97d2843e5a17
Satan1c <satan1c@ukr.net>
2023-07-07 19:12:35 +0300

    update ticket list format

--- interactions/commands/list-command.js
+++ interactions/commands/list-command.js
@@ -4,12 +4,11 @@ export default async function (interaction) {
 			'Список тикетов:\n' +
 			`${[...tickets.entries()].reduce(
 				(p, [userId, ticket], index) =>
-					`${p}\n${index + 1}. ` +
-					`Взят: **${ticket.active ? 'Да' : 'Нет'}** ` +
-					`Пользователь: <@${userId}> (\`${userId}\`) ` +
-					`Тикет:
-					 [сообщение](https://discord.com/channels/${mainGuild.id}/${ticketsChannel.id}/${ticket.thread})
-					 (<#${ticket.thread}>)`,
+					`${p}\n\n${index + 1}.
+					[сообщение](https://discord.com/channels/${mainGuild.id}/${ticketsChannel.id}/${ticket.thread})
+					(<#${ticket.thread}>)` +
+					`  - Взят: **${ticket.active ? 'Да' : 'Нет'}** ` +
+					`  - Пользователь: <@${userId}> (\`${userId}\`)`,
 				''
 			)}`,
 		flags: 'SuppressEmbeds',
