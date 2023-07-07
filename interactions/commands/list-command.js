059975a4014744c505e02a9492d25e94b637e812
Satan1c <satan1c@ukr.net>
2023-07-07 16:20:34 +0300

    more readable

--- interactions/commands/list-command.js
+++ interactions/commands/list-command.js
@@ -1,14 +1,17 @@
 export default async function (interaction) {
 	interaction.reply({
-		content: `Список тикетов:\n${[...tickets.entries()].reduce(
-			(p, [u, t], i) =>
-				`${p}\n${i + 1}. Взят: **${
-					t.active ? 'Да' : 'Нет'
-				}** Пользователь: <@${u}> (\`${u}\`) Тикет: https://discord.com/channels/${mainGuild.id}/${ticketsChannel.id}/${
-					t.thread
-				}(<#${t.thread}>)`,
-			''
-		)}`,
+		content:
+			'Список тикетов:\n' +
+			`${[...tickets.entries()].reduce(
+				(p, [userId, ticket], index) =>
+					`${p}\n${index + 1}. ` +
+					`Взят: **${ticket.active ? 'Да' : 'Нет'}** ` +
+					`Пользователь: <@${userId}> (\`${userId}\`) ` +
+					`Тикет:
+					 [сообщение](https://discord.com/channels/${mainGuild.id}/${ticketsChannel.id}/${ticket.thread})
+					 (<#${ticket.thread}>)`,
+				''
+			)}`,
 		flags: 'SuppressEmbeds',
 	});
 }
