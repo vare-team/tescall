031ec662348556007eda02d2834f42bd45b9d603
Satan1c <satan1c@ukr.net>
2023-07-25 19:02:19 +0300

    list command update fix

--- interactions/commands/list-command.js
+++ interactions/commands/list-command.js
@@ -2,9 +2,9 @@ export default async function (interaction) {
 	interaction.reply({
 		content: `${tickets.size > 0 ? 'Список тикетов:\n' : 'Тикетов нет'}${[...tickets.entries()].reduce(
 			(p, [userId, ticket], index) =>
-				`${p}${index + 1}.
-					[сообщение](https://discord.com/channels/${mainGuild.id}/${ticketsChannel.id}/${ticket.thread})
-					(<#${ticket.thread}>)` +
+				`${p}${index + 1}. ` +
+				`[сообщение](https://discord.com/channels/${mainGuild.id}/${ticketsChannel.id}/${ticket.thread})` +
+				`(<#${ticket.thread}>)` +
 				`  - Взят: **${ticket.active ? 'Да' : 'Нет'}** ` +
 				`  - Пользователь: <@${userId}> (\`${userId}\`) ` +
 				'\n\n',
