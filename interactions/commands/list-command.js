ec51778ceecf1d85fedb92af3ad8722e14046c9c
Maxim <roscom.123@yandex.ru>
2024-10-05 17:54:40 +1000

    fix "application not respond" with a large number of tickets

--- interactions/commands/list-command.js
+++ interactions/commands/list-command.js
@@ -1,6 +1,12 @@
+import declension from '../../utils/declension.js';
+
 export default async function (interaction) {
-	interaction.reply({
-		content: `${tickets.size > 0 ? 'Список тикетов:\n' : 'Тикетов нет'}${[...tickets.entries()].reduce(
+	const ticketEntries = [...tickets.entries()];
+	const maxTickets = 4;
+	const remainingTickets = ticketEntries.length - maxTickets;
+	let ticketList = ticketEntries
+		.slice(0, maxTickets)
+		.reduce(
 			(p, [userId, ticket], index) =>
 				`${p}${index + 1}. ` +
 				`[сообщение](https://discord.com/channels/${mainGuild.id}/${ticketsChannel.id}/${ticket.thread})` +
@@ -9,7 +15,12 @@ export default async function (interaction) {
 				`\n  - Пользователь: <@${userId}> (\`${userId}\`) ` +
 				'\n\n',
 			''
-		)}`,
+		);
+
+	if (remainingTickets > 0)
+		ticketList += `И ещё ${remainingTickets} ${declension(remainingTickets, ['тикет', 'тикета', 'тикетов'])}`;
+	interaction.reply({
+		content: `${tickets.size > 0 ? 'Список тикетов:\n' : 'Тикетов нет'}${ticketList}`,
 		flags: 'SuppressEmbeds',
 	});
 }
